d3ef02860414f21c710f3093844aed4b8a3419ae
Nicolas Lopes <03nicolas.lopes@gmail.com>
2025-12-27 12:13:01 -0300

    Delete projeto_social/projeto_social-main/projeto_social-main/projetosocial directory

--- projeto_social/projeto_social-main/projeto_social-main/projetosocial/bancodedadosmultimeios.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE Livros
-ALTER COLUMN cdd TYPE CHAR(20)
-USING cdd::CHAR(20);
-ALTER TABLE exemplares ADD COLUMN livro_cdd CHAR(20) NOT NULL;
-ALTER TABLE exemplares ADD CONSTRAINT fk_exemplares_livros
-FOREIGN KEY (livro_cdd) REFERENCES Livros(cdd);
